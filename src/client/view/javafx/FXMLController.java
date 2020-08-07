0b9eda5d2d412d93391b294ce6baac9b7dc91802
saahilkumar <60633758+saahilkumar@users.noreply.github.com>
2020-08-07 12:10:32 -0400

    adding chat bubbles

--- src/client/view/javafx/FXMLController.java
+++ src/client/view/javafx/FXMLController.java
@@ -7,6 +7,7 @@ import java.awt.TextField;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckMenuItem;
@@ -15,7 +16,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.scene.web.HTMLEditor;
@@ -104,23 +108,42 @@ public class FXMLController {
     // split message by space
     String[] words = msg.split(" ");
 
+    StackPane bubble = new StackPane();
+    HBox surface = new HBox();
+    surface.setPadding(new Insets(5, 5, 5, 5));
+
     for (String word : words) {
       // if the word equals an emoji name (ex. <3) then replace it with an ImageView of the emoji
       // if the word equals a twitch emoji (ex. PepeHands) then replace it with an ImageView of the twitch emoji
       // otherwise just add the word as plaintext
       if (MultiChatView.EMOTES.containsKey(word.trim())) {
         ImageView emoji = getEmote("/client/resources/images/emojis/" + MultiChatView.EMOTES.get(word.trim()), 20);
-        Platform.runLater(() -> chatLog.getChildren().add(emoji));
+//        Platform.runLater(() -> chatLog.getChildren().add(emoji));
+        surface.getChildren().add(emoji);
       } else if (MultiChatView.TWITCH_EMOTES.containsKey(word.trim())) {
         ImageView twitchEmote = getEmote("/client/resources/images/twitch/" + MultiChatView.TWITCH_EMOTES.get(word.trim()), 40);
-        Platform.runLater(() -> chatLog.getChildren().add(twitchEmote));
+//        Platform.runLater(() -> chatLog.getChildren().add(twitchEmote));
+        surface.getChildren().add(twitchEmote);
       } else {
         Text txt = new Text(word + " ");
         txt.setFill(c);
-        Platform.runLater(() -> chatLog.getChildren().add(txt));
+//        Platform.runLater(() -> chatLog.getChildren().add(txt));
+        surface.getChildren().add(txt);
       }
     }
+    Rectangle rect = new Rectangle();
+    rect.setX(0);
+    rect.setY(0);
+    rect.setWidth(surface.prefWidth(-1));
+    rect.setHeight(surface.prefHeight(-1));
+    System.out.println(surface.getWidth() + " " + surface.getHeight());
+    System.out.println(surface.prefWidth(-1));
+    rect.setArcWidth(20);
+    rect.setArcHeight(20);
+    rect.setFill(Color.CORNFLOWERBLUE);
 
+    bubble.getChildren().addAll(rect, surface);
+    Platform.runLater(() -> chatLog.getChildren().add(bubble));
     Platform.runLater(() -> chatLog.getChildren().add(new Text("\n")));
   }
 
